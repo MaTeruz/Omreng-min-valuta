9082249a173f967ed66e79fe706fc5ba0cd744e8
Martin Ladefoged <Martin Ladefoged@DESKTOP-OGQMLSP>
2021-09-20 09:44:21 +0200

    Add project files.

---
+++ Omreng min valuta/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Omreng_min_valuta
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Euro = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.USD = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.DKK = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.NOK = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SEK = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Beløb";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Euro";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Euro
+            // 
+            this.Euro.AutoSize = true;
+            this.Euro.Location = new System.Drawing.Point(87, 85);
+            this.Euro.Name = "Euro";
+            this.Euro.Size = new System.Drawing.Size(38, 15);
+            this.Euro.TabIndex = 4;
+            this.Euro.Text = "label3";
+            this.Euro.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "USD";
+            // 
+            // USD
+            // 
+            this.USD.AutoSize = true;
+            this.USD.Location = new System.Drawing.Point(87, 116);
+            this.USD.Name = "USD";
+            this.USD.Size = new System.Drawing.Size(38, 15);
+            this.USD.TabIndex = 6;
+            this.USD.Text = "label5";
+            this.USD.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "DKK";
+            // 
+            // DKK
+            // 
+            this.DKK.AutoSize = true;
+            this.DKK.Location = new System.Drawing.Point(87, 146);
+            this.DKK.Name = "DKK";
+            this.DKK.Size = new System.Drawing.Size(38, 15);
+            this.DKK.TabIndex = 8;
+            this.DKK.Text = "label7";
+            this.DKK.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "NOK";
+            // 
+            // NOK
+            // 
+            this.NOK.AutoSize = true;
+            this.NOK.Location = new System.Drawing.Point(87, 177);
+            this.NOK.Name = "NOK";
+            this.NOK.Size = new System.Drawing.Size(38, 15);
+            this.NOK.TabIndex = 10;
+            this.NOK.Text = "label9";
+            this.NOK.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(42, 210);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 15);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "SEK";
+            // 
+            // SEK
+            // 
+            this.SEK.AutoSize = true;
+            this.SEK.Location = new System.Drawing.Point(87, 210);
+            this.SEK.Name = "SEK";
+            this.SEK.Size = new System.Drawing.Size(44, 15);
+            this.SEK.TabIndex = 12;
+            this.SEK.Text = "label11";
+            this.SEK.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(147, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(147, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(147, 202);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.SEK);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.NOK);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.DKK);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.USD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Euro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label USD;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label DKK;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label NOK;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Euro;
+        private System.Windows.Forms.Label NKK;
+        private System.Windows.Forms.Label SEK;
+    }
+}
+
---
+++ Omreng min valuta/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Omreng_min_valuta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();         
+        }
+        
+        public string[] getMoneyRates()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string webData = wc.DownloadString("http://api.exchangeratesapi.io/v1/latest?access_key=1719511ede98606b617fa68112d65398");
+
+            //Convert data to JSON represented in C#
+            JObject radar = JObject.Parse(webData);
+
+            //int[] newArray = new int[array.Length];
+
+            string[] money =
+            {
+                radar.SelectToken("rates").SelectToken("EUR").ToString(),
+                radar.SelectToken("rates").SelectToken("USD").ToString(),
+                radar.SelectToken("rates").SelectToken("DKK").ToString(),
+                radar.SelectToken("rates").SelectToken("NOK").ToString(),
+                radar.SelectToken("rates").SelectToken("SEK").ToString()
+            };
+            return money;
+        }
+
+        public void updatemig(float guld, string currency)
+        {
+
+            //label5.Text = newRates[0];
+            string[] newRates = getMoneyRates(); {}            
+            
+
+            float[] rates = new float[newRates.Length];
+            for (int i = 0; i < newRates.Length; i++)
+            {
+                rates[i] = float.Parse(newRates[i]);
+            }
+            
+            //Code prep fra array
+            float dollers = float.Parse(newRates[1]);
+            float dkKrone = float.Parse(newRates[2]);
+            float nkKrone = float.Parse(newRates[3]);
+            float skKrone = float.Parse(newRates[4]);
+
+            //code prep fra array
+            if (currency == "USD")  {guld = (guld / dollers);}
+            if (currency == "DKK")  {guld = (guld / dkKrone);}
+            if (currency == "NOK")  {guld = (guld / nkKrone);}
+            if (currency == "SEK")  {guld = (guld / skKrone);}
+
+            //Dis Euro
+            Euro.Text = guld.ToString();
+            Euro.Visible = true;
+
+            //Euro Til USD            
+            float uDol;
+            uDol = (dollers * guld);
+            USD.Text = uDol.ToString();
+            USD.Visible = true;
+
+            //Euro til DKK
+            float Kro;
+            Kro = (dkKrone * guld);
+            DKK.Text = Kro.ToString();
+            DKK.Visible = true;
+
+            //Euro til NOK
+            float nKro;
+            nKro = (nkKrone * guld);
+            NOK.Text = nKro.ToString();
+            NOK.Visible = true;
+
+            //Euro til SEK
+            float sKro;
+            sKro = (skKrone * guld);
+            SEK.Text = sKro.ToString();
+            SEK.Visible = true;
+        }
+
+        //Euro
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text )) 
+            {
+                //definere input fra textbox1
+                float input = float.Parse(textBox1.Text);
+                //kalder vores CLEAN function kald!!!
+                updatemig(input, "");
+            }
+        }
+
+        //USD
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                //definere input fra textbox1
+                float input = float.Parse(textBox2.Text);          
+                //kalder vores CLEAN function kald!!!
+                updatemig(input, "USD");
+            }
+        }
+
+        //DKK
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {                
+                //definere input fra textbox1
+                float input = float.Parse(textBox3.Text);
+                //kalder vores CLEAN function kald!!!
+                updatemig(input, "DKK");
+            }
+        }
+
+        //NOK
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                //definere input fra textbox1
+                float input = float.Parse(textBox4.Text);
+                //kalder vores CLEAN function kald!!!
+                updatemig(input, "NOK");
+            }
+        }
+
+        //SEK
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox5.Text))
+            {
+                float input = float.Parse(textBox5.Text);
+                //kalder vores CLEAN function kald!!!
+                updatemig(input, "SEK");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
